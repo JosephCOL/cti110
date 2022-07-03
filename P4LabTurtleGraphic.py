5cfab57aac7bb45967d4500a1fd47b89e5bc94d5
JosephCOL <107326960+JosephCOL@users.noreply.github.com>
2022-07-02 22:01:14 -0400

    P4LAB2a
    Small shape drawing script

---
+++ P4LabTurtleGraphic.py
@@ -0,0 +1,14 @@
+import turtle
+win = turtle.Screen()
+t = turtle.Turtle()
+
+for i in (1, 2 ,3 , 4):
+    t.forward(50)
+    t.left(90)
+
+for i in (1, 2, 3):
+    t.forward(50)
+    t.right(120)
+
+
+win.mainloop()
